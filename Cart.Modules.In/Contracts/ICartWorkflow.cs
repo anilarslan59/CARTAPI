e705411b72b65f4caf13c2606e61dd914a59c89f
anilarslan59 <anilarslan59@gmail.com>
2021-01-03 01:46:45 +0300

    Add files via upload

---
+++ Cart.Modules.In/Contracts/ICartWorkflow.cs
@@ -0,0 +1,13 @@
+﻿using Cart.DataModels.RequestModels;
+using Cart.DataModels.ResponseModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cart.Modules.In.Contracts
+{
+    public interface ICartWorkflow
+    {
+        AddCartResponseModel Add(AddCartListRequestModel model);
+    }
+}
